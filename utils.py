36f22b8ca3502eab9dfa1da10969a2517f455fc6
paulrojasg <paul.rojas@correounivalle.edu.co>
2024-03-24 16:03:35 -0500

    Added hashing and checking passwords functions

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from jwt import decode_token
 from db import get_user
+from bcrypt import gensalt, hashpw, checkpw
 
 
 """
@@ -23,3 +24,49 @@ def is_login(token):
                     return True
 
     return False
+"""
+Hashes password given by input
+
+@type raw_password: str
+@param raw_password: Password to be hashed
+@rtype: str
+@returns: Hashed password
+
+@author: Paul Rodrigo Rojas G. (paul.rojas@correounivalle.edu.co)
+"""
+
+def hash_password(raw_password):
+    try:
+        bytes = raw_password.encode('utf-8')
+        salt = gensalt()
+
+        hash = hashpw(bytes, salt).decode('utf-8')
+
+        return hash
+
+    except Exception:
+        return None
+
+
+"""
+Check if given raw guessed password match hashed password
+
+@type guessed_password: str
+@param guessed_password: Raw password to be checked
+@type hashed_password: str
+@param hashed_password: Hashed password
+@rtype: Boolean
+@returns: True -> passwords match, False -> passwords do not match
+
+@author: Paul Rodrigo Rojas G. (paul.rojas@correounivalle.edu.co)
+"""
+
+
+def check_password(guessed_password, hashed_password):
+    try:
+        guessed_bytes = guessed_password.encode('utf-8')
+        hash_bytes = hashed_password.encode('utf-8')
+
+        return checkpw(guessed_bytes, hash_bytes)
+    except Exception:
+        return False
