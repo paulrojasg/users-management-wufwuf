9c1d7ea2e3773774bcaf8f53e5d4a1671fdd3bdc
paulrojasg <paul.rojas@correounivalle.edu.co>
2024-03-24 16:05:30 -0500

    Edited is_login function to return user data when valid

--- utils.py
+++ utils.py
@@ -8,22 +8,25 @@ Checks if user is login using the provided jwt token
 
 @type token: str
 @param token: token to be checked
-@rtype: Boolean
-@returns: True -> User is login, False -> User is not login
+@rtype: dict or None
+@returns: dict (user data)-> User login, None-> User is not login
 
 @author: Paul Rodrigo Rojas G. (paul.rojas@correounivalle.edu.co)
 """
 
 def is_login(token):
-    decoded_data = decode_token(token)
-    if decoded_data:
-        for key in decoded_data:
-            if key == 'username':
-                username = decoded_data['username']
-                if get_user(username):
-                    return True
+    if token:
+        decoded_data = decode_token(token)
+        if decoded_data:
+            for key in decoded_data:
+                if key == 'username':
+                    username = decoded_data['username']
+                    user = get_user(username)
+                    if user:
+                        return user
 
-    return False
+    return None
+
 """
 Hashes password given by input
 
