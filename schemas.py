b0a40222c9ec880c50ea5a026816b815366adedd
paulrojasg <paul.rojas@correounivalle.edu.co>
2024-03-24 21:22:12 -0500

    Added documentation comments to schemas and created schema for validating editing user data

--- schemas.py
+++ schemas.py
@@ -1,10 +1,37 @@
 from pydantic import BaseModel
 
+
+"""
+Schema used for validating login data
+
+@author: Paul Rodrigo Rojas G. (paul.rojas@correounivalle.edu.co)
+"""
 class LoginDataSchema(BaseModel):
     username: str
     password: str
 
+
+"""
+Schema used for validating creating user form
+
+@author: Paul Rodrigo Rojas G. (paul.rojas@correounivalle.edu.co)
+"""
 class CreateUserSchema(BaseModel):
+    username: str
+    email: str
+    password: str
+    age: str | None
+    name: str | None
+    lastname: str | None
+    role: str
+
+
+"""
+Schema used for validating edit user form
+
+@author: Paul Rodrigo Rojas G. (paul.rojas@correounivalle.edu.co)
+"""
+class EditUserSchema(BaseModel):
     username: str
     email: str
     password: str
